718b2ccfdf5f4d58731d6079bcdd03ae76853eaa
carlosrrdev <rr.carlos.dev@gmail.com>
2022-07-17 10:18:51 -0500

    Create Thought model

---
+++ models/Thoughts.js
@@ -0,0 +1,61 @@
+// Create Thought model
+const { Schema, model, Types } = require("mongoose");
+
+// ReactionSchema
+const ReactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: "Please enter a reaction",
+    trim: true,
+    maxLength: [200, "Exceeded character limit of 200"],
+  },
+  username: {
+    type: String,
+    required: "Provide your username",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Thought schema
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "Provide a thought",
+      trim: true,
+      maxLength: [200, "Exceeded character limit of 200"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: "Provide your username",
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Grab reaction count for a Thought
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+// Create Thought model
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+// Import models
+const User = require("./User");
+const Thought = require("./Thoughts");
+
+module.exports = { User, Thought };
